159639b938524f344e868fbaeb72f01e53b8ebd5
Arlo <67204708+Watto1337@users.noreply.github.com>
2021-07-08 12:37:12 -0700

    Update Slider.py

--- Slider.py
+++ Slider.py
@@ -25,9 +25,16 @@ class Slider():
         self.colour = colour
 
         self.name = Slider.text.render(name, True, colour, (0,0,0))
-        self.nameRect = self.name.get_rect()
 
         if array != None: array.append(self)
+
+    def set(self, val):
+        self.val = val
+
+        if self.reversed:
+            self.n = self.min + self.max - self.val
+        else:
+            self.n = self.val
 
     def adjust(self, mousePos):
         if mousePos[0] > self.pos[0] - Slider.sensitivity and mousePos[0] < self.pos[0] + self.width + Slider.sensitivity and \
